2c0e9f84796ec607faaed5b3ed4bc20d1eb09a23
Unknown-3447 <Kieranborthwick@mmc.school.nz>
2024-05-24 10:29:36 +1200

    Add files via upload

--- C_03_integer_checker.py.py
+++ C_03_integer_checker.py.py
@@ -1,20 +1,23 @@
-
 # Checks that users enter an integer
 # that is more than 13
-while True:
+def int_check():
+    while True:
 
-    error = "Please enter an integer that is 13 or more."
+        error = "Please enter an integer that is 13 or more."
 
-    try:
-        my_num = int(input("Enter an integer"))
+        try:
+            response = int(input("Enter an integer"))
 
-        # checks that the number is more than / equal to 13
-        if my_num < 13:
+            # checks that the number is more than / equal to 13
+            if response < 13:
+                print(error)
+            else:
+                return response
+
+        except ValueError:
             print(error)
-        else:
-            print("Your number ", my_num)
 
-        print("You number is ", my_num)
 
-    except ValueError:
-        print(error)
+# main routine goes here
+target_score = int_check()
+print(target_score)
